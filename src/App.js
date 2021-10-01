5f56c06fc01197789f4b3a7f4558e9874e43323b
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-10-01 12:06:19 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import { useEffect, useState } from 'react';
+import {Row, Spinner} from 'react-bootstrap';
+import './App.css';
+import News from './Componenets/News/News';
+
+function App() {
+  
+  return (
+    <div className="App">
+   
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componenets/News/News.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const News = (props) => {
+    const{title,urlToImage,description,}=props.news;
+    return (
+        
+    );
+};
+
+export default News;
